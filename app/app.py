ef0487e5d520baf70e588746090cd0662fb2a3d9
sarah-altowaity1 <sa5793@nyu.edu>
2022-12-16 14:52:21 -0500

    add user object and locat object function

--- app/app.py
+++ app/app.py
@@ -14,9 +14,34 @@ from werkzeug.security import check_password_hash
 
 ################## setup ##################
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+# set up flask-login for user authentication
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+################## routes ##################
+config = dotenv_values(".env")
+
+# turn on debugging if in development mode
+if config['FLASK_ENV'] == 'development':
+    # turn on debugging, if in development
+    app.debug = True  # debug mnode
 
 
+db = pymongo.MongoClient(host='db', port=27017)
 
+# a class to represent a user
+class User(flask_login.UserMixin):
+    # inheriting from the UserMixin class gives this blank class default implementations of the necessary methods that flask-login requires all User objects to have
+    # see some discussion of this here: https://stackoverflow.com/questions/63231163/what-is-the-usermixin-in-flask
+    def __init__(self, data):
+        '''
+        Constructor for User objects
+        @param data: a dictionary containing the user's data pulled from the database
+        '''
+        self.id = data['_id'] # shortcut to the _id field
+        self.data = data # all user data from the database is stored within the data field
 
 
 
@@ -33,16 +58,14 @@ def locate_user(user_id=None, username=None):
         # loop up by email
         criteria = {"username": username}
     doc = db.users.find_one(criteria) # find a user with this email
-    # if user exists in he database, create a User object and return it
-    if doc:
-        # return a user object representing this user
+    if doc: 
+                # return a user object representing this user
         user = User(doc)
         return user
-
+    # else
     return None
-
+    
 ################## routes ##################
-
 ################## run server ##################
 if __name__=='__main__':
     app.run(port=3000)
