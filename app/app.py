df91c8964ed372a03a150219b4446aa87db6adf9
sarah-altowaity1 <sa5793@nyu.edu>
2022-12-16 14:52:09 -0500

    add user object and locat object function

--- app/app.py
+++ app/app.py
@@ -17,9 +17,31 @@ app = Flask(__name__)
 
 
 
+
+
+
+def locate_user(user_id=None, username=None):
+    '''
+    Return a User object for the user with the given id or email address, or None if no such user exists.
+    @param user_id: the user_id of the user to locate
+    @param email: the email address of the user to locate
+    '''
+    if user_id:
+        # loop up by user_id
+        criteria = {"_id": ObjectId(user_id)}
+    else:
+        # loop up by email
+        criteria = {"username": username}
+    doc = db.users.find_one(criteria) # find a user with this email
+    # if user exists in he database, create a User object and return it
+    if doc:
+        # return a user object representing this user
+        user = User(doc)
+        return user
+
+    return None
+
 ################## routes ##################
-
-
 
 ################## run server ##################
 if __name__=='__main__':
