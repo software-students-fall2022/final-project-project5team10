595fbca010893d3e4226595597cfb55d7b92aeca
sarah-altowaity1 <sa5793@nyu.edu>
2022-12-16 14:37:02 -0500

    add dependencies for the app

--- app/app.py
+++ app/app.py
@@ -1,4 +1,16 @@
 from flask import Flask, jsonify, render_template, request, redirect, flash
+from dotenv import dotenv_values
+from werkzeug.utils import secure_filename
+
+import pymongo
+import datetime
+from bson.objectid import ObjectId
+import sys, os
+
+# modules useful for user authentication
+import flask_login
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 
 ################## setup ##################
 app = Flask(__name__)
